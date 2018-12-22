05275c1fcf4b142f676d260cf8e665357d75266d
Terrencezzj <blandiel.zhao@mail.utoronto.ca>
2018-12-22 06:24:51 -0500

    add wxpy

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 Can be used to get information of the price, turnover, capitalization of stocks.
+
+Dependencies:
+	iexfinance
+	rasa_nlu
+	spacy
+	wxpy
+
+Usage:
+	Just run the main.py
+	You need to login in the wechat by QRcode.
+	The conversation initially holds in file_helper, but you can easily change it by modified @bot.register(bot.file_helper) to friend or other chat windows. 
--- main.py
+++ main.py
@@ -7,7 +7,12 @@ from rasa_nlu.training_data import load_data
 from rasa_nlu.model import Trainer
 from rasa_nlu import config
 import spacy
+from wxpy import *
 
+
+############################################################
+# Initialization part                                      #
+############################################################
 
 # Define the states
 INIT = 0
@@ -35,14 +40,16 @@ weather = random.choice(weathers)
 
 
 # Create a trainer that uses this config
-trainer = Trainer(config.load("config_spacy.yml"))
+trainer = Trainer(config.load("config\config_spacy.yml"))
 
 # Load the training data
-training_data = load_data('demo-rasa-stock.json')
+training_data = load_data('data\demo-rasa-stock.json')
 
 # Create an interpreter by training the model
 interpreter = trainer.train(training_data)
 
+# Initialize bot
+bot = Bot(console_qr=True, cache_path=True)
 
 # Define the policy rules
 policy_rules = {
@@ -50,11 +57,11 @@ policy_rules = {
     (INIT, "phone_number"): (AUTHED, "Perfect, welcome back! Choose the stock you want to know.", None),
     (AUTHED, "default"): (AUTHED, "Would you like to know the information about AAPL or TSLA?", CHOOSE_STOCK),
     (AUTHED, "negative"): (CHOOSE_OTHER_STOCK, "Ok, tell me the stock you want to choose", None),
-    (AUTHED, "specify_stock"): (CHOOSE_STOCK, "Perfect, trying to get the infromation. What kind information you want to know?", CHOOSE_QUE),
+    (AUTHED, "specify_stock"): (CHOOSE_STOCK, "Perfect, trying to get the infromation", CHOOSE_QUE),
     (CHOOSE_STOCK, "default"): (CHOOSE_STOCK,
                                 "Would you like to know about the price or market capitalization?", CHOOSE_QUE),
     (CHOOSE_STOCK, "specify_stock"): (CHOOSE_STOCK,
-                                      "Ok, add this to the stock list. What kind information you want to know?",
+                                      "Ok, add this to the stock list. What kind of information you want to know?",
                                       CHOOSE_QUE),
     (CHOOSE_STOCK, "negative"): (CHOOSE_OTHER_QUE, "Ok, then I guess you want to know the turnover, right?", None),
     (CHOOSE_STOCK, "specify_question"): (CHOOSE_QUE, "Get that!", None),
@@ -116,7 +123,17 @@ responses = {
                 'That is beyond the scope of my knowledge.']}
 
 
+##################################################################
+# Utility functions                                              #
+##################################################################
+
 def negated_ents(phrase, ent_vals):
+    """
+    Find negated entities in the input message
+    :param phrase: a string of message
+    :param ent_vals: entity values
+    :return: dictionary of entities with boolean value
+    """
     ents = [e for e in ent_vals if e in phrase]
     ends = sorted([phrase.index(e) + len(e) for e in ents])
     start = 0
@@ -135,8 +152,12 @@ def negated_ents(phrase, ent_vals):
     return result
 
 
-# Pending state transitions
 def interpret(message):
+    """
+    get intent of input message
+    :param message: a string of message
+    :return: a string of intent
+    """
     # Declare global var
     global stocks
     global stocks_reader
@@ -147,13 +168,13 @@ def interpret(message):
 
     # match phone number
     match = re.search('(\d{3}[-\.\s]??\d{3}[-\.\s]??\d{4}|\(\d{3}\)\s*\d{3}[-\.\s]??\d{4}|\d{3}[-\.\s]??\d{4})',
-                       message)
+                      message)
     if match is not None:
         return 'phone_number'
 
     # Extract the entities
     interpreted = interpreter.parse(message)
-    # print(interpreted)
+    print(interpreted)
     entities = interpreted["entities"]
     intent = interpreted["intent"]['name']
     ent_vals = [e["value"] for e in entities]
@@ -170,25 +191,21 @@ def interpret(message):
         nlp = spacy.load('en_core_web_md')
         doc = nlp(message)
         possible_pos = ["NOUN", "PROPN"]
-        new_stocks = []
         for ent in doc:
             if ent.pos_ in possible_pos \
                     and re.search('(-|[A-Z]){2,8}', ent.text) is not None \
                     and ent.text not in stocks:
-                new_stocks.append(ent.text)
+                stocks.append(ent.text)
         # deal with some special symbol
         match_special = re.findall('[A-Z]+-[A-Z]?\*?', message)
         if len(match_special) > 0:
-            new_stocks.extend(match_special)
+            stocks.extend(match_special)
         if re.search(' A$|^A$|A and', message) is not None and 'A' not in stocks:
-            new_stocks.append("A")
-        if len(new_stocks) == 0:
-            return 'default'
-        stocks.extend(new_stocks)
+            stocks.append("A")
         stocks = list(set(stocks))
         # read stocks
         if len(stocks) > 0:
-            # print(stocks)
+            print(stocks)
             for stock in stocks:
                 try:
                     Stock(stock).get_market_cap()
@@ -220,8 +237,15 @@ def interpret(message):
     return 'default'
 
 
-# Define chitchat_response()
+##################################################################
+# Unrelated chatting functions                                   #
+##################################################################
 def chitchat_response(message):
+    """
+    response to chitchat
+    :param message: a string of message
+    :return: a string of response
+    """
     # Call match_rule()
     response, phrase = match_rule(rules, message)
     # Return none is response is "default"
@@ -236,6 +260,12 @@ def chitchat_response(message):
 
 
 def match_rule(rules, message):
+    """
+    match rules in message and give response
+    :param rules: a dictionary of rules
+    :param message: a string of massage
+    :return: a string of response
+    """
     for pattern, responses in rules.items():
         match = re.search(pattern, message)
         if match is not None:
@@ -245,8 +275,12 @@ def match_rule(rules, message):
     return "default", None
 
 
-# Define basic_respond(message):
 def basic_respond(message):
+    """
+    basic level response of robot
+    :param message: a string of massage
+    :return: a string of response
+    """
     if message.endswith("?"):
         # Check if the message is in the responses
         if message in responses:
@@ -262,7 +296,11 @@ def basic_respond(message):
 
 
 def replace_pronouns(message):
-
+    """
+    replace pronouns with different person
+    :param message: a string of massage
+    :return: a string of replaced massage
+    """
     message = message.lower()
     if ' me' in message:
         return re.sub('me', 'you', message)
@@ -277,24 +315,34 @@ def replace_pronouns(message):
     return message
 
 
-# Define send_message()
+##################################################################
+# Controller functions                                           #
+##################################################################
 def send_message(state, pending, message):
+    """
+    Transition of Finite State Machine
+    :param state: a string of state
+    :param pending: a string of pending state
+    :param message: a string of message
+    :return: new state
+    """
     global stocks
     global stocks_reader
     global question
     global repeat
+    res = ''
     intent = interpret(message)
     print(state, intent, pending)
     if intent == "unavailable":
-        print("BOT : {}".format("There is no available stock symbol I can find, please capitalize the symbol"))
+        res += "{}".format("There is no available stock symbol I can find, please capitalize the symbol\n")
     # Deal with chitchat
     if ((state, intent) not in policy_rules) or intent == 'default':
         if intent == 'greet':
-            print("BOT : {}".format('Hi ' + username + '!'))
-            return state, pending
+            res += "{}\n".format('Hi!')
+            return state, pending, res
         response = chitchat_response(message)
         if response is not None and intent != "unavailable":
-            print("BOT : {}".format(response))
+            res += "{}\n".format(response)
         pending = None
         new_state, response, pending_state = policy_rules[(state, "default")]
         # Only repeat the requirement first time in the same state
@@ -303,64 +351,66 @@ def send_message(state, pending, message):
         else:
             repeat = 0
         if repeat < 2 or (state == CHOOSE_STOCK and intent == 'specify_stock'):
-            print("BOT : {}".format(response))
+            res += "{}\n".format(response)
         if pending_state is not None:
             pending = (pending_state, "default")
-        return new_state, pending
+        return new_state, pending, res
 
     # Calculate the new_state, response, and pending_state
     new_state, response, pending_state = policy_rules[(state, intent)]
-    print("BOT : {}".format(response))
-    if intent == 'negative':
-        intent = 'default'
-    if new_state == CHOOSE_OTHER_QUE:
-        question = "turnover"
+    res += "{}\n".format(response)
+    print(21, new_state, response, pending_state)
+
+    # Pending state transitions
     if pending is not None:
         if (pending[0] == AUTHED and intent == 'phone_number') \
                 or (pending[0] == CHOOSE_STOCK and intent == 'specify_stock') \
-                or (pending[0] == CHOOSE_QUE and intent == 'specify_question') \
-                or new_state == CHOOSE_OTHER_QUE:
-            if new_state == CHOOSE_QUE or new_state == CHOOSE_OTHER_QUE:
-                if question == "price":
-                    if len(stocks) > 1:
-                        for i in range(len(stocks)):
-                            print("BOT : The price of {} is {}".format(
-                                stocks_reader.get_company_name()[stocks[i]],
-                                stocks_reader.get_price()[stocks[i]]))
-                    else:
-                        print("BOT : The price of {} is {}".format(
-                            stocks_reader.get_company_name(),
-                            stocks_reader.get_price()))
-                if question == "capitalization":
-                    if len(stocks) > 1:
-                        for i in range(len(stocks)):
-                            print("BOT : The market capitalization  of {} is {}".format(
-                                stocks_reader.get_company_name()[stocks[i]],
-                                stocks_reader.get_market_cap()[stocks[i]]))
-                    else:
-                        print("BOT : The market capitalization  of {} is {}".format(
-                            stocks_reader.get_company_name(),
-                            stocks_reader.get_market_cap()))
-                if question == "turnover":
-                    if len(stocks) > 1:
-                        for i in range(len(stocks)):
-                            print("BOT : The turnover of {} is {}".format(
-                                stocks_reader.get_company_name()[stocks[i]],
-                                stocks_reader.get_volume()[stocks[i]]))
-                    else:
-                        print("BOT : The turnover of {} is {}".format(
-                            stocks_reader.get_company_name(),
-                            stocks_reader.get_volume()))
+                or (pending[0] == CHOOSE_QUE and intent == 'specify_question'):
+            print(22, pending[0], new_state, question)
+            # print question answer
+            if question == "price":
+                if len(stocks) > 1:
+                    for i in range(len(stocks)):
+                        res += "The price of {} is {}\n".format(
+                            stocks_reader.get_company_name()[stocks[i]],
+                            stocks_reader.get_price()[stocks[i]])
+                else:
+                    res += "The price of {} is {}\n".format(
+                        stocks_reader.get_company_name(),
+                        stocks_reader.get_price())
+            if question == "capitalization":
+                if len(stocks) > 1:
+                    for i in range(len(stocks)):
+                        res += "The market capitalization  of {} is {}\n".format(
+                            stocks_reader.get_company_name()[stocks[i]],
+                            stocks_reader.get_market_cap()[stocks[i]])
+                else:
+                    res += "The market capitalization  of {} is {}\n".format(
+                        stocks_reader.get_company_name(),
+                        stocks_reader.get_market_cap())
+            if question == "turnover":
+                if len(stocks) > 1:
+                    for i in range(len(stocks)):
+                        res += "The turnover of {} is {}\n".format(
+                            stocks_reader.get_company_name()[stocks[i]],
+                            stocks_reader.get_volume()[stocks[i]])
+                else:
+                    res += "The turnover of {} is {}\n".format(
+                        stocks_reader.get_company_name(),
+                        stocks_reader.get_volume())
             new_state, response, pending_state = policy_rules[pending]
             if response != "":
-                print("BOT : {}".format(response))
+                res += "{}\n".format(response)
             else:
                 new_state, response, pending_state = policy_rules[(new_state, "default")]
-                print("BOT : {}".format(response))
+                res += "{}\n".format(response)
             intent = "default"
+
     if pending_state is not None:
         pending = (pending_state, intent)
-    return new_state, pending
+
+    print(23, new_state, pending, res)
+    return new_state, pending, res
 
 
 # Define send_messages()
@@ -373,35 +423,53 @@ def send_messages(messages):
         # print(state, pending, msg)
 
 
+@bot.register(bot.file_helper, except_self=False)
+def reply_self(msg):
+    global state
+    global pending
+    print(0)
+    if msg.type != 'Text':
+        print(1)
+        response = "[奸笑][奸笑]I don't understand images"
+    elif msg.text == 'quit':
+        response = " Bye!"
+        state = INIT
+        pending = None
+    else:
+        print(2)
+        print(msg.text, state, pending)
+        state, pending, response = send_message(state, pending, msg.text)
+        print(3, state, pending, response)
+    bot.file_helper.send('BOT:' + response)
+
+
+##################################################################
+# Main                                                           #
+##################################################################
 if __name__ == '__main__':
-    username = input("Enter your username: ")
-    print("Now start chat! Enter 'quit' to end")
-    print("BOT : Hello, I'm stock helper, you can use phone number to log in.")
+    bot.file_helper.send("BOT: Hello, I'm stock helper, you can use phone number to log in.")
     # Initialize state and pending
     state = INIT
     pending = None
-    message = input(username + ": ")
-    while message != 'quit':
-        state, pending = send_message(state, pending, message)
-        message = input(username + ": ")
+    embed()
+    # message = input(username + ": ")
+    # while message != 'quit':
+    #     state, pending = send_message(state, pending, message)
+    #     message = input(username + ": ")
 
 
-
-
-# FOR TEST
+##################################################################
+# FOR TEST                                                       #
+##################################################################
 # send_messages([
 #     "what's your name?",
 #     "55s-12345",
-#     "do you remember when I ordered 1000 kilos by accident?",
 #     "my phone number is 555-12345",
 #     "No",
-#     "A",
 #     "i'm looking for A",
 #     "AAPL",
 #     "QTUMUSDT",
 #     "price",
-#     "No, I don't want to know the price and market capitalization",
-#     "No, neither",
 #     "No, show me the turnover",
 #     "market cap"
 # ])
